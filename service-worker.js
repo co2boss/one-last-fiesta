b7e83109e58989ec4156e0d6ba76ddbe11b4c5d2
co2boss <dtello94@gmail.com>
2026-07-08 17:27:34 -0400

    Add files via upload
    v0.2 - Add Fiesta Passport experience

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,4 @@
-const CACHE='fiesta-v1';const FILES=['./','./index.html','./style.css','./app.js','./manifest.json','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='fiesta-v02';
+const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
